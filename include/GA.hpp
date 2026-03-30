f9949abe96aa5be5ac551ff1e6fabd0e76fbcd35
davidwu110405 <davidwu110405@gmail.com>
2026-03-31 00:59:17 +0800

    add: GA_Params(yet impliment), single chromosome to single ranged value, single variable fitness function.

--- include/GA.hpp
+++ include/GA.hpp
@@ -5,12 +5,44 @@
 #include <iostream>
 #include <bitset>
 #include <random>
+#include <math.h>
+
 namespace GA{
+enum class SelMethods{
+    BEST,
+    RANDOM,
+    ROULETTE_DEFAULT,
+    ROULETTE_RANK,
+};
+
+enum class CrossMethods{
+    SINGLE_POINT,
+    MULTI_POINT,
+};
+
+enum class MutMethods{
+    CONST,
+    DECREASE,
+};
+
+struct GA_Params{
+             int population_size;
+             int generation_num;
+      SelMethods selection_method;
+           float crossover_prob;
+    CrossMethods crossover_method;
+           float mutation_prob;
+      MutMethods mutation_method;
+};
+
 template<size_t N>
 struct Population{
+    /*Data*/
     std::vector<std::bitset<N>> chrosomes;
     std::vector<float> fitness_values;
+    const ulong range_chromosome = std::pow(2, int(N));
 
+    /*Random generator*/
     static std::mt19937& get_random_engine(){
         static std::random_device rd;
         static std::mt19937 gen(rd());
@@ -39,11 +71,23 @@ int mutation(Population<N>& chromos, float mut_rate){
             if(dist(gen)){
                 chromo.flip(i);
             };
-        }
+        };
     };
     return 0;
 };/*int mutation*/
 
-}/*namespace GA*/;
+template<size_t N, typename T>
+double chromosome_to_value(std::bitset<N>& chromo, ulong range_input, T bound_upper, T bound_lower){
+    double range_output = static_cast<double>(bound_upper - bound_lower);
+    double input = static_cast<double>(chromo.to_ulong());
+    double range_input_double = static_cast<double>(range_input);
+    return input / range_input_double * range_output + bound_lower;
+};/*double chromosome_to_value*/
+
+template<typename Func, typename T>
+float fitness_value(Func f, T x){
+    return 1/(1+std::abs(f(x)));
+};/*float fitness_value*/
+};/*namespace GA*/
 
 #endif
--- test/test_GA.cpp
+++ test/test_GA.cpp
@@ -1,5 +1,9 @@
 #include "GA.hpp"
 
+float Xby2(float x){
+    return std::pow(x,2);
+};
+
 int main(){
     int population_size = 10000;
     std::cout << "Basic population initialization (size = " << population_size << "):" << '\n';
@@ -10,10 +14,18 @@ int main(){
 
     float mutation_rate = 0.05;
     GA::mutation(population, mutation_rate);
-    std::cout << "Mutation test(mutation rate = " << mutation_rate << "):" << '\n';
+    std::cout << "\nMutation test(mutation rate = " << mutation_rate << "):" << '\n';
     for(int i=0; i<10; i++){
         std::cout << "population[" << i << "]=" << population.chrosomes[i] << '\n';
-    }
+    };
+
+    std::cout << "\nChromosome to value test:" << '\n';
+    for(int i=0; i<10; i++){
+        double value = GA::chromosome_to_value(population.chrosomes[i], population.range_chromosome, 10, -10);
+        std::cout << "population[" << i << "]=" << value << "|fitness value:" << GA::fitness_value(Xby2, value) << '\n';
+    };
+
+    std::cout << "\nFitness value test:" << GA::fitness_value(Xby2, 2) << '\n';
 
     std::cout << '\n' << "End of test." << '\n';
     return 0;
